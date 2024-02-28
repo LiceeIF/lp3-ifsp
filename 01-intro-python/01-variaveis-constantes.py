edc140a24c2d5fd0d970fd1820e4452fee0de457
Alice Maria <alice.maria@aluno.ifsp.edu.br>
2024-02-28 14:33:55 -0300

    adição do primeiro conteúdo

---
+++ 01-intro-python/01-variaveis-constantes.py
@@ -0,0 +1,42 @@
+# Identificadores
+# letras, numeros e _
+# não pode ser plavra reservada: if, None, True, False
+# Case sensitive
+nome = "Pedro"
+Nome = "Pedro"
+
+# Variáveis
+# Tudo em minúsculo
+# separador _
+# snake_case
+
+idade = 20
+pessoa_fisica = "Marcio"
+pessoa_juridica = "Paulo LTDA"
+imposto_renda = 2220.45
+
+# E o tipo?
+# em Java, se declara String nome = "Pedro"; int idade = 20; etc.
+# em Python, existe a inferência de tipo, o tipo será definido com base no valor (literal)
+
+# Constantes
+# Não existe constante em python mas existem convenções
+# Convenção: nome em maiúsculo
+PI = 3.14
+
+# Comentários de única linha
+
+'''
+comentários de
+múltiplas linhas
+'''
+
+# docstring (comentário de documentação)
+# documentar classe, módulos, funções...
+
+def somar (double numero1, numero2):
+    return numero1 + numero2
+
+# para uma função funcionar, é necessário que sua identação esteja completa
+
+somar()
