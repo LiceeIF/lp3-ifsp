9a16d8c59cbc2235e56acb147a91c13bfb39c6fc
LiceeIF <a.alicemaria.oliveira@gmail.com>
2024-03-15 10:33:25 -0300

    subindo exercicios

--- exercicios/exercicio-04.py
+++ exercicios/exercicio-04.py
@@ -21,11 +21,11 @@ Crie uma função em Python que implementa essa verificação
 # precisa verificar se há 4 números após o BR
 # precisa verificar se há 1 letra X ou Y como último caracter
 
-identificador = input('insira seu identificador')
-
-while len(identificador) == 7 and identificador[0] == 'B' and identificador[1] == 'R':
-    if identificador[2].isnumeric and identificador[3].isnumeric and identificador[4].isnumeric and identificador[5].isnumeric:
-        print('seu identificador é válido')
+def verificar_identificador(identificador):
+    if len(identificador) == 7 and identificador[0:2] == 'BR' and identificador[2:6].isnumeric() and (identificador[6] == 'X' or identificador[6] == 'Y'):
+        print('Seu identificador é válido')
     else:
-        print('seu identificador é inválido')
-break
+        print('Seu identificador é inválido')
+
+identificador = input('Insira seu identificador: ')
+verificar_identificador(identificador)
